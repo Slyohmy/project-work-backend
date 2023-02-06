22182350a8d876d598441f5947c3e0f5215d8191
slyohmy <simon.westlin@hotmail.com>
2023-02-06 23:13:57 +0100

    added cross origin to each controller

--- src/main/java/projectwork/backend/controller/TestController.java
+++ src/main/java/projectwork/backend/controller/TestController.java
@@ -2,12 +2,14 @@ package projectwork.backend.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/test")
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Tag(name = "Test")
 public class TestController {
 
